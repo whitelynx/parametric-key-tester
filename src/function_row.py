7cb28f030df83ff31575704d1b957dc14b3f4a6f
David H. Bronke <whitelynx@gmail.com>
2021-04-06 22:58:27 -0400

    feat(function_row): added alignment tabs and control box

--- src/function_row.py
+++ src/function_row.py
@@ -3,9 +3,9 @@ import argparse
 from os.path import abspath, dirname, join
 
 from solid import cube, rotate, scad_render_to_file
-from solid.utils import up, down, left, right, forward
+from solid.utils import up, down, left, right, forward, back
 
-from board_mount import pro_micro
+from board_mount import BoardMount, pro_micro, m2_shaft_radius
 from key_grid_tester import (
     default_wall_height,
     key_grid_tester,
@@ -17,6 +17,108 @@ from switch_plate import (
     mount_width,
     mount_length,
 )
+from utils import cylinder_outer
+
+
+distribution_board = BoardMount(18.3, 30, 1.7, has_connector=False)
+
+
+def control_box(wall_length, wall_width, wall_height):
+    max_board_thickness = max(pro_micro.board_thickness, distribution_board.board_thickness)
+
+    board_surface_pos = max(20 + wall_thickness - wall_length, 5)
+
+    case_length = (
+        board_surface_pos
+        + max_board_thickness
+        + 3
+        + wall_thickness
+    )
+    case_width = (
+        pro_micro.board_length + distribution_board.board_length + 3 * wall_thickness
+    )
+
+    board_offset_forward = (
+        board_surface_pos
+        + max_board_thickness
+        - case_length / 2
+    )
+
+    def position_control_box(part):
+        """Position the control box on the final model."""
+        #return left(case_width / 2)(
+        return left(wall_width / 4 - case_width / 2)(
+            forward((wall_length + case_length) / 2)(
+                up(wall_height / 2)(part)
+            )
+        )
+
+    def position_controller(part):
+        """Position the controller board within the control box."""
+        return forward(board_offset_forward)(
+            left(case_width / 2 - wall_thickness + wall_thickness / 4)(
+                rotate((90, -90, 0))(
+                    part
+                )
+            )
+        )
+
+    def position_distribution_board(part):
+        """Position the distribution board within the control box."""
+        return forward(board_offset_forward)(
+            right(case_width / 2 - wall_thickness + wall_thickness / 4)(
+                rotate((90, 90, 0))(
+                    part
+                )
+            )
+        )
+
+    center_post_length = max_board_thickness + 2.5
+    center_post_offset_right = (
+        case_width / 2
+        - wall_thickness * 1.5
+        - distribution_board.board_length
+    )
+
+    inner = (
+        down(wall_thickness / 2 + 1)(
+            back(wall_thickness + 1)(
+                cube(
+                    (
+                        case_width - 2 * wall_thickness,
+                        case_length + 2,
+                        wall_height - wall_thickness + 1,
+                    ),
+                    center=True,
+                )
+            )
+        )
+        + rotate((90, 0, 0))(
+            right(center_post_offset_right)(
+                cylinder_outer(4.5, 100)
+            )
+        )
+    )
+
+    case = position_control_box(
+        cube((case_width, case_length, wall_height), center=True)
+        - inner
+        # Center post (between the two boards):
+        + forward(board_surface_pos - case_length / 2 + center_post_length / 2)(
+            right(center_post_offset_right)(
+                cube((wall_thickness * 2, center_post_length, wall_height), center=True)
+                - rotate((90, 0, 0))(
+                    cylinder_outer(m2_shaft_radius, center_post_length)
+                )
+            )
+        )
+        - position_controller(pro_micro.board_profile(0))
+        - position_distribution_board(distribution_board.board_profile(0))
+    )
+
+    cutout = position_control_box(inner)
+
+    return (case, cutout)
 
 
 def function_row(
@@ -49,18 +151,42 @@ def function_row(
             up(board_up)(forward(board_forward)(rotate((0, -90, 90))(part)))
         )
 
+    control_box_case, control_box_cutout = control_box(
+        wall_length, wall_width, wall_height
+    )
+
     full_case = (
         case
+        - control_box_cutout
+        + control_box_case
         - left(wall_width / 2 - 20)(
             cube((15, 1 * y_grid_size * 2, 23 * 2), center=True)
         )
-        - position_board(pro_micro.board_profile(3))
+        #- position_board(pro_micro.board_profile(3))
     )
 
     big_block = cube((200, 200, 200), center=True)
+    center_offset = x_grid_size / 2 if width_units % 2 else 0
+
+    alignment_tab = cube((wall_thickness, wall_thickness / 2, wall_height - wall_thickness), center=True)
+    alignment_tabs = right(center_offset)(
+        up((wall_height - wall_thickness) / 2)(
+            forward(wall_length / 2 - wall_thickness)(alignment_tab)
+            + back(wall_length / 2 - wall_thickness)(alignment_tab)
+        )
+    )
+
     return (
-        full_case - right(100 - (x_grid_size / 2 if width_units % 2 else 0))(big_block),
-        full_case - left(100 + (x_grid_size / 2 if width_units % 2 else 0))(big_block),
+        (
+            full_case
+            - right(100 + center_offset)(big_block)
+            + alignment_tabs
+        ),
+        (
+            full_case
+            - left(100 - center_offset)(big_block)
+            - alignment_tabs
+        ),
     )
 
 
